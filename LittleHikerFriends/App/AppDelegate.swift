47434cfaf75d4e2653236f7f464a3ac8cfbf8a80
Kkugy <tjdrnrdla@hanmail.net>
2025-07-27 23:39:06 +0900

    Info.plist 추가 및 수정

--- LittleHikerFriends/App/AppDelegate.swift
+++ LittleHikerFriends/App/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // Naver SDK 설정
-        NMFAuthManager.shared().ncpKeyId = Naver.clientID
+//        NMFAuthManager.shared().ncpKeyId = Naver.clientID
 
         return true
     }
