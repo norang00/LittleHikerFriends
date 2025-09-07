ce89cd6cc6441e1f24fdeb33b8cf882961aaf41d
Kkugy <tjdrnrdla@hanmail.net>
2025-09-07 16:44:09 +0900

    Feat: ProfileDraft Model 구현

---
+++ LittleHikerFriends/App/CreateProfileView/Model/ProfileDraft.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileDraft.swift
+//  LittleHikerFriends
+//
+//  Created by sungkug_apple_developer_ac on 9/7/25.
+//
+import UIKit
+
+struct ProfileDraft {
+    var avatar: UIImage?
+    var nickname: String
+}
